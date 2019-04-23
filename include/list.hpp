d4b8a8984ac5c06600de8c860c44373577ca82b0
Michael Latushko <ancord.m@yandex.ru>
2019-04-23 17:30:12 +0300

    Ничего нового

--- include/list.hpp
+++ include/list.hpp
@@ -9,15 +9,27 @@ struct ListNode
 };
 
 template <typename T, typename Allocator = std::allocator<T>>
-class List
+struct List
 {
 	using allocator_type = Allocator;
-	using pointer = typename allocator_type::pointer;
+	using size_type = std::size_t;
+	using value_type = ListNode<T>;
+	using reference = value_type&;
+	using const_reference = const value_type&;
+	using pointer = std::allocator_traits<Allocator>::pointer;
+	using const_pointer = std::allocator_traits<Allocator>::const_pointer;
 
 	//typedef Allocator allocator_type;
 	allocator_type a;
 	pointer p;
-public:
+
+
+/*	explicit List(const allocator_type &a = allocator_type())
+	{
+
+	}
+*/
+
 	List() : a( Allocator() )
 	{
 		
@@ -34,3 +46,13 @@ public:
 		return p;
 	}
 };
+
+
+/*
+Как бы я сделал Лист без аллокатора:
+1. В конструкторе провел бы только инициализацию
+2. Ввел бы метод reserve или работал бы без него
+3. В методе push выделял бы память под новый элемент или 
+просто складывал бы его в готовую ячейку. Тут нужен указатель
+4. В ходе обхода смещал бы указатель по хранилищу и вытаскивал элемент. Нужен итератор
+*/
